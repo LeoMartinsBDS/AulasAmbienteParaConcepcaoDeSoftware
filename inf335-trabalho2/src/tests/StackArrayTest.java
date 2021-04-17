c6b52bfbecadb14389a0e5743aed258270478e03
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2021-04-17 17:45:34 -0300

    Teste unitários
    Teste unitários em operações em uma pilha

---
+++ inf335-trabalho2/src/tests/StackArrayTest.java
@@ -0,0 +1,122 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import algorithms.datastructures.StackArray;
+
+public class StackArrayTest {
+
+	private StackArray stack;
+	
+	@BeforeEach
+	public void initialize() {
+		this.stack = new StackArray();
+	}
+	
+	@Test
+	@DisplayName("Teste de pilha vazia")
+	public void testarPilhaVazia() {
+		var result = stack.isEmpty();
+		assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Teste de pilha cheia")
+	public void testarSePilhaCheia() {
+		
+		for(int i = 0; i <= 9; i++) {
+			stack.push(i);
+		}
+		
+		var result = stack.isFull();
+		assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Teste de remoção e adição de elementos na pilha")
+	public void testarRemocaoEInsercaoElementos() {
+		
+		for(int i = 0; i <= 9; i++) {
+			stack.push(i);
+		}
+		
+		for(int i = 9; i >= 0; i--) {
+			var retorno = stack.pop();
+			assertEquals(retorno, i);
+		}
+	}
+	
+	@Test
+	@DisplayName("Teste de peek apos a remoção de elementos na pilha")
+	public void testarPeekAposRemocao() {
+		
+		for(int i = 0; i <= 9; i++) {
+			stack.push(i);
+		}
+		
+		for(int i = 9; i >= 0; i--) {
+			stack.pop();
+			var retorno = stack.peek();
+			assertEquals(retorno, i-1);
+		}
+		
+	}
+	
+	@Test
+	@DisplayName("Teste de peek para pilha vazia")
+	public void testarPeekPilhaVazia() {
+		var retorno = stack.peek();
+		assertEquals(retorno, -1);
+	}
+	
+	@Test
+	@DisplayName("Teste de peek para pilha limpa")
+	public void testarSePilhaLimpa() {
+		for(int i = 0; i <= 11; i++) {
+			stack.push(i);
+		}
+		
+		stack.makeEmpty();
+		var retorno = stack.isEmpty();
+		assertTrue(retorno);
+	}
+	
+	@Test
+	@DisplayName("Teste do método que obtem tamanho da pilha")
+	public void testarTamanhoPilha() {
+	
+		var elementos = new int[]{1,4,6,3,9};
+		
+		for(int i = 0; i <= 4; i++) {
+			stack.push(elementos[i]);
+		}
+	
+		var tamanhoPilha = stack.size();
+		
+		assertEquals(tamanhoPilha, elementos.length);
+	}
+	
+	@Test
+	@DisplayName("Teste de remoção de mais elementos que foram adicionados na pilha")
+	public void testarRemocaoElementosMaiorQuePilha() {
+		
+		int retorno = 0;
+		
+		for(int i = 0; i <= 4; i++) {
+			stack.push(i);
+		}
+		
+		for(int i = 8; i >= 0; i--) {
+			retorno = stack.pop();
+		}
+		
+		assertEquals(retorno, -1);
+	}
+	
+	
+}
